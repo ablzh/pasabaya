ca47d3667eb0f663b0603445fc16d38fcbe22ace
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-24 19:29:52 +0800

    Update subs controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -4,9 +4,13 @@ class SubscribersController < ApplicationController
   def create
     @subscriber = Subscriber.new(subscriber_params)
     if @subscriber.save
-      redirect_to root_path, notice: "Thanks for subscribing!"
+      # Отправляем письмо! .deliver_later делает это асинхронно
+      UserMailer.welcome_email(@subscriber).deliver_later
+
+      redirect_to root_path, notice: "Вы успешно подписаны! Письмо уже в пути (проверьте Mailtrap)."
     else
-      redirect_to root_path, alert: "Error in email or name."
+      # Если валидация не прошла, нам нужно передать ошибки обратно на главную
+      render "pages/home", status: :unprocessable_entity
     end
   end
 
