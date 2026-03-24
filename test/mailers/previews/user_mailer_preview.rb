d9eaa61fc760756fa637682432ad4eb87a3026ba
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-24 21:21:33 +0800

    Update home page. Email sanding and flash works

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def welcome_email
+    UserMailer.welcome_email(Subscriber.new(name: "Тестовый Юзер", email: "example@example.com"))
+  end
 end
