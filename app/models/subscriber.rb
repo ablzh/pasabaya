8d4ab0b148bba09d34cdd08cb75d354f55783657
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-25 08:52:53 +0800

    Check checkbox before subscribing is checked

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,7 @@
 class Subscriber < ApplicationRecord
+  attr_accessor :terms_accepted
+  validates :terms_accepted, acceptance: true
+
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
