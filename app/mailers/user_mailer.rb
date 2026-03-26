41f8681dcf9b286f0bee37c13438f5d8d4777f1f
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-26 11:29:13 +0800

    Set up base email sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,4 @@
 class UserMailer < ApplicationMailer
-  default from: "hello@yourproject.com" # Твой будущий адрес
-
   def welcome_email(subscriber)
     @subscriber = subscriber
     mail(to: @subscriber.email, subject: "Welcome to Pasabaya.app! Let’s face the energy crisis together 🚗")
