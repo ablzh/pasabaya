4546c4fecb01824287ae21644f1f67209a575a1d
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-26 09:14:09 +0800

    Update email text and layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(subscriber)
     @subscriber = subscriber
-    mail(to: @subscriber.email, subject: 'Thanks for subscribing!')
+    mail(to: @subscriber.email, subject: 'Welcome to Pasabaya.app! Let’s face the energy crisis together 🚗')
   end
 end
