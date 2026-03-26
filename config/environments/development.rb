bf80856f7ade451ed41ca6c87a18e82976ca12b9
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-26 11:56:15 +0800

    Update development key name for mailtrap sandbox

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: Rails.application.credentials.dig(:mailtrap, :user_name),
-    password: Rails.application.credentials.dig(:mailtrap, :password),
+    user_name: Rails.application.credentials.dig(:mailtrap_sandbox, :user_name),
+    password: Rails.application.credentials.dig(:mailtrap_sandbox, :password),
     address: "sandbox.smtp.mailtrap.io",
     host: "sandbox.smtp.mailtrap.io",
     port: "2525",
