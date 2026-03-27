2b5ef86863117d5934c4412163b19527514d2023
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-27 13:16:55 +0800

    Update email template and user mailer for support unsub

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
-    UserMailer.welcome_email(Subscriber.new(name: "Тестовый Юзер", email: "example@example.com"))
+    subscriber = Subscriber.first || Subscriber.new(name: "Test User", email: "example@example.com")
+    UserMailer.welcome_email(subscriber)
   end
 end
