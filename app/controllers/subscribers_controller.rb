a92e45f7e6da835fd64d40b180576f8f61da4b77
ablzh <123565843+ablzh@users.noreply.github.com>
2026-03-27 09:39:43 +0800

    Add  notification from a form without reloading

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -10,10 +10,13 @@ class SubscribersController < ApplicationController
   def create
     @subscriber = Subscriber.new(subscriber_params)
     if @subscriber.save
-      # Отправляем письмо! .deliver_later делает это асинхронно
       UserMailer.welcome_email(@subscriber).deliver_later
+      flash.now[:notice] = "You've successfully subscribed for updates! The email is on its way."
 
-      redirect_to root_path, notice: "You've successfully subscribed for updates! The email is on its way."
+      respond_to do |format|
+        format.turbo_stream
+        format.html {redirect_to root_path, notice: flash[:notice]}
+      end
     else
       render "pages/home", status: :unprocessable_entity
     end
