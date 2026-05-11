7aa88ea0d5c472b17a275a553496bf767ffa7423
ablzh <123565843+ablzh@users.noreply.github.com>
2026-05-11 20:40:07 +0800

    Add Profile Fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_27_044311) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_11_123941) do
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "subscribers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
@@ -18,4 +27,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_27_044311) do
     t.datetime "unsubscribed_at"
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "facebook_profile_url"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
