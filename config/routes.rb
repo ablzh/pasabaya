6672f7d9e5cccc423f85219de9dc1d5d2edd2836
ablzh <123565843+ablzh@users.noreply.github.com>
2026-05-11 20:39:18 +0800

    Generate authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "privacy", to: "pages#privacy"
 
   get "up" => "rails/health#show", as: :rails_health_check
