0e1ba4d54bd573f5c932c2cf4cd3adf0ea914156
ablzh <123565843+ablzh@users.noreply.github.com>
2026-06-01 19:40:34 +0800

    Create seeds with biggest cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Create the Country
+philippines = Location.find_or_create_by!(
+  name: "Philippines",
+  location_type: :country,
+  country_code: "PH"
+)
+
+# 2. Create Regions
+regions = [
+  { name: "Metro Manila", location_type: :region, parent: philippines },
+  { name: "Central Visayas", location_type: :region, parent: philippines },
+  { name: "Davao Region", location_type: :region, parent: philippines }
+]
+
+regions.each do |region_data|
+  Location.find_or_create_by!(region_data)
+end
+
+# 3. Create Key Cities
+cities = [
+  # Metro Manila Cities
+{ name: "Manila", location_type: :city, parent: Location.find_by(name: "Metro Manila") },
+{ name: "Quezon City", location_type: :city, parent: Location.find_by(name: "Metro Manila") },
+{ name: "Makati", location_type: :city, parent: Location.find_by(name: "Metro Manila") },
+
+# Central Visayas Cities
+{ name: "Cebu City", location_type: :city, parent: Location.find_by(name: "Central Visayas") },
+{ name: "Mandaue City", location_type: :city, parent: Location.find_by(name: "Central Visayas") },
+
+# Davao Region Cities
+{ name: "Davao City", location_type: :city, parent: Location.find_by(name: "Davao Region") }
+]
+
+cities.each do |city_data|
+  Location.find_or_create_by!(city_data)
+  end
+
+puts "Seeded #{Location.count} locations."
