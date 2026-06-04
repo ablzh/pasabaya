28b2182340bae8f7708bba6683e91f8d11915132
ablzh <123565843+ablzh@users.noreply.github.com>
2026-06-04 14:54:12 +0800

    Add terms page and update privacy page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  allow_unauthenticated_access only: %i[ home privacy ]
+  allow_unauthenticated_access only: %i[ home privacy terms ]
   def home
   end
 
   def privacy
+  end
+
+  def terms
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   get "privacy", to: "pages#privacy"
+  get "terms", to: "pages#terms"
 
   get "up" => "rails/health#show", as: :rails_health_check
-
+  
   root "pages#home"
   resources :subscribers, only: [ :create ] do
     member do
