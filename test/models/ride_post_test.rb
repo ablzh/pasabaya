ce2cba58274d3910b8c5e2792fd08092e4ea1253
ablzh <123565843+ablzh@users.noreply.github.com>
2026-06-07 13:18:17 +0800

    Create tests

--- test/models/ride_post_test.rb
+++ test/models/ride_post_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class RidePostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid if departure time is in the past" do
+    # You can fetch a post from your ride_posts.yml fixture
+    ride_post = ride_posts(:one)
+
+    # Set the departure time to 1 hour ago
+    ride_post.departure_time = 1.hour.ago
+
+    assert_not ride_post.valid?
+    assert_includes ride_post.errors[:departure_time], "can't be in the past"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,26 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email_address: " DOWNCASED@EXAMPLE.COM ")
     assert_equal("downcased@example.com", user.email_address)
   end
+  # 1. Accessing fixture data to test custom methods
+  test "initials returns capitalized first letters of first and last name" do
+    user = users(:one) # "Juan Dela Cruz"
+    assert_equal "JD", user.initials
+  end
+
+  # 2. Testing validations (negative path)
+  test "invalid without facebook_profile_url" do
+    user = users(:one)
+    user.facebook_profile_url = nil
+
+    assert_not user.valid?
+    assert_includes user.errors[:facebook_profile_url], "can't be blank"
+  end
+
+  # 3. Testing validations (positive path)
+  test "valid with correct facebook_profile_url format" do
+    user = users(:one)
+    user.facebook_profile_url = "https://facebook.com/custom_username"
+
+    assert user.valid?
+  end
 end
